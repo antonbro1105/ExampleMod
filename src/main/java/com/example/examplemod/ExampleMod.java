ba5f2f55ff99b19f82694d0585bbaae7f4cc38b7
antonbro1105 <anton.1105@yandex.ru>
2026-01-29 18:57:05 +0300

    Update ExampleMod.java

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,171 +1,108 @@
-package com.example.examplemod;
+package net.fabricmc.example;
 
-import com.example.examplemod.commands.ExampleQuietCommand;
-import com.example.examplemod.commands.ExampleShowCommand;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.init.Blocks;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.client.render.entity.SpiderEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.*;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.boss.BossBar;
+import net.minecraft.entity.boss.ServerBossBar;
+import net.minecraft.entity.mob.CaveSpiderEntity;
+import net.minecraft.entity.mob.SpiderEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBucketMilk;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTBase;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
-import net.minecraft.nbt.NBTTagString;
-import net.minecraft.network.play.server.S36PacketSignEditorOpen;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.tileentity.TileEntitySign;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.world.storage.SaveHandler;
-import net.minecraftforge.common.DimensionManager;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.ServerChatEvent;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+public class ExampleMod implements ModInitializer, ClientModInitializer {
+    public static final String MOD_ID = "spidermod";
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
+    public static final EntityType<GiantSpiderBoss> GIANT_SPIDER = Registry.register(
+            Registries.ENTITY_TYPE, new Identifier(MOD_ID, "giant_spider"),
+            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, GiantSpiderBoss::new)
+                    .dimensions(EntityDimensions.fixed(7.0f, 4.5f))
+                    .build()
+    );
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION , acceptableRemoteVersions = "*")
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final Item SPIDER_BOSS_SPAWN_EGG = Registry.register(
+            Registries.ITEM, new Identifier(MOD_ID, "spider_boss_spawn_egg"),
+            new SpawnEggItem(GIANT_SPIDER, 0x342D27, 0xFF0000, new Item.Settings())
+    );
 
-    public static final String aqua = EnumChatFormatting.RESET + "" + EnumChatFormatting.AQUA;
-    public static final String pink = EnumChatFormatting.RESET + "" + EnumChatFormatting.LIGHT_PURPLE;
-
-
-    public static ArrayList<String> quietList;
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
-        System.out.println("GRAVEL BLOCK >> " + Blocks.gravel.getUnlocalizedName());
-
-        MinecraftForge.EVENT_BUS.register(this);
-        FMLCommonHandler.instance().bus().register(this);
-
-        quietList = new ArrayList<String>();
+    @Override
+    public void onInitialize() {
+        FabricDefaultAttributeRegistry.register(GIANT_SPIDER, GiantSpiderBoss.createAttributes());
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
+            content.add(SPIDER_BOSS_SPAWN_EGG);
+        });
     }
 
-    @EventHandler
-    public void serverLoad(FMLServerStartingEvent event)
-    {
-        event.registerServerCommand(new ExampleQuietCommand());
-        event.registerServerCommand(new ExampleShowCommand());
-    }
-
-
-    //I'm a bit of test code and has no real purpose being here... :) But it works!
-    @SubscribeEvent
-    public void onPlayerInteractEvent(PlayerInteractEvent event) {
-      /*  if (!event.world.isRemote && event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK && event.entityPlayer.getHeldItem() == null
-                && event.entityPlayer.isSneaking()) {
-            final TileEntity te = event.world.getTileEntity(event.pos);
-            if (te != null && te instanceof TileEntitySign) {
-                if (event.entityPlayer instanceof EntityPlayerMP) {
-                    ((EntityPlayerMP) event.entityPlayer).playerNetServerHandler
-                            .sendPacket(new S36PacketSignEditorOpen(te.getPos()));
-                    addMessage("Hello SIGN!", (EntityPlayerMP) event.entityPlayer);
+    @Override
+    public void onInitializeClient() {
+        EntityRendererRegistry.register(GIANT_SPIDER, (context) -> 
+            new SpiderEntityRenderer(context) {
+                @Override
+                protected void setupTransforms(SpiderEntity spider, MatrixStack matrices, float animProg, float yaw, float delta) {
+                    super.setupTransforms(spider, matrices, animProg, yaw, delta);
+                    matrices.scale(5.0F, 5.0F, 5.0F);
                 }
-
             }
-        }*/
+        );
     }
 
-    @SubscribeEvent
-    public void onEntityJoin(PlayerEvent.PlayerLoggedInEvent event){
+    public static class GiantSpiderBoss extends SpiderEntity {
+        private final ServerBossBar bossBar = new ServerBossBar(
+                Text.literal("Король Пауков"), BossBar.Color.RED, BossBar.Style.NOTCHED_10);
 
-
-            System.out.println("Login Player found!");
-            EntityPlayer player = event.player;
-            System.out.println(player.getName() + " joined!");
-
-            SaveHandler saveHandler = (SaveHandler) DimensionManager.getWorld(0).getSaveHandler();
-            File file = new File(saveHandler.getWorldDirectory(), "/playerdata/" + player.getGameProfile().getId().toString() + ".dat");
-
-            if(!file.exists()) {
-                if( event.player.worldObj.isRemote ) return;
-
-                //TODO: Clean UP this Mess!!!
-                Item item = Item.getItemById(320);
-                ItemStack is = new ItemStack(item, 5);
-
-             //   is.setStackDisplayName("Welcome Food!");
-                NBTTagCompound display = new NBTTagCompound();
-                if( is.hasTagCompound()) {
-                    System.out.println("Display Tag Found");
-                    display = is.getTagCompound().getCompoundTag("display");
-                }
-                else {
-                    System.out.println("Creating Tags");
-                    is.setTagCompound(new NBTTagCompound());
-                    is.setTagInfo("display", new NBTTagCompound());
-                    display = is.getTagCompound().getCompoundTag("display");
-                    display.setTag("Name",new NBTTagString("Welcome Food!"));
-                    display.setTag("Lore", new NBTTagCompound());
-                }
-
-                NBTTagList list = display.getTagList("Lore", 9);
-
-                list.appendTag(new NBTTagString(aqua+"Enjoy this food!"));
-                list.appendTag(new NBTTagString(pink+"I hope you are ready to catch"));
-                list.appendTag(new NBTTagString(pink+"all the pokemon!"));
-
-                display.setTag("Lore", list);
-                display.setTag("Name",new NBTTagString("Welcome Food!"));
-                is.getTagCompound().setTag("display", display);
-                //is.setTagInfo("display",new NBTTagString("{display:{Name:\"name here\",color:0x5555F,Lore:[\"lore here\", \"lore here\"]}}"));
-                player.inventory.addItemStackToInventory(is);
-
-                player.addChatMessage(new ChatComponentText("\2478**\247cWelcome\247f to our server, " + player.getName()));
-            }
-            else {
-                player.addChatMessage(new ChatComponentText("\2478**\247cWelcome\247f back! " + player.getName()));
-            }
-
-
-    }
-
-    @SubscribeEvent
-    public void onPlayerChatEvent(ServerChatEvent event) {
-
-        EntityPlayerMP sender = event.player;
-        event.setCanceled(true);
-
-        if(quietList.contains(sender.getUniqueID().toString())){
-            addMessage("You have quieted chat. Use /show to see it again.",sender);
-            return;
+        public GiantSpiderBoss(EntityType<? extends SpiderEntity> type, World world) {
+            super(type, world);
+            this.bossBar.setDarkenSky(true);
         }
 
-
-        List<EntityPlayerMP> players = MinecraftServer.getServer().getConfigurationManager().playerEntityList;
-        for (EntityPlayerMP receiver : players)
-        {
-            if(!quietList.contains(receiver.getUniqueID().toString()))
-                addMessage( sender.getName() + " say '" + event.message, receiver);
-
+        public static DefaultAttributeContainer.Builder createAttributes() {
+            return SpiderEntity.createSpiderAttributes()
+                    .add(EntityAttributes.GENERIC_MAX_HEALTH, 200.0)
+                    .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 4.0)
+                    .add(EntityAttributes.GENERIC_ARMOR, 15.0)
+                    .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 1.0);
         }
 
-    }
+        @Override
+        public void mobTick() {
+            super.mobTick();
+            this.bossBar.setPercent(this.getHealth() / this.getMaxHealth());
+            if (!this.getWorld().isClient && this.getHealth() < 60.0f && this.age % 100 == 0) {
+                for (int i = 0; i < 2; i++) {
+                    CaveSpiderEntity minion = EntityType.CAVE_SPIDER.create(this.getWorld());
+                    if (minion != null) {
+                        minion.refreshPositionAndAngles(this.getX() + random.nextGaussian(), this.getY(), this.getZ() + random.nextGaussian(), 0, 0);
+                        this.getWorld().spawnEntity(minion);
+                    }
+                }
+            }
+        }
 
-    public static void addMessage(String msg,EntityPlayerMP ep) {
-        ep.addChatMessage(new ChatComponentText("\2478[\247cMODCHAT\2478] \247f" + msg));
+        @Override
+        public void onStartedTrackingBy(ServerPlayerEntity player) {
+            super.onStartedTrackingBy(player);
+            this.bossBar.addPlayer(player);
+        }
+
+        @Override
+        public void onStoppedTrackingBy(ServerPlayerEntity player) {
+            super.onStoppedTrackingBy(player);
+            this.bossBar.removePlayer(player);
+        }
     }
 }
